9ea28ff669825991826b245b683f48672fcb0ef6
SevenZero <iamseventoodyai708090sz@gmail.com>
2024-10-22 08:31:48 +0700

    some anim

--- Assets/Scripts/ResultScript.cs
+++ Assets/Scripts/ResultScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class ResultScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.tag == "BORDER")
+        {
+            ObjectPooling.instance.ReturnToPool(gameObject.name, gameObject);
+        }
     }
 }
